f46e679e9418c610eef9ef3a8ba00ee933630286
¨kericho¨ <lvisrono90@gmail.com>
2022-12-13 11:04:55 +0300

    made some changes on the file

--- js/index.js
+++ js/index.js
@@ -1,120 +1,74 @@
-document.addEventListener("DOMContentLoaded", function(){
-        
-    const url = "https://api.npoint.io/0fd99609eb72596d7c69/films/"
-    const filmsDiv = document.querySelector("#films")
-    const posterDiv = document.querySelector("#poster")
-    const showingDiv = document.querySelector("#showing")
-    const tktsBtn = showingDiv.querySelector('.ui.orange.button')
+let url = 'http://localhost:3000/films'
+const listHolder = document.getElementById('films')
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementsByClassName('film item')[0].remove()
+    fetchMovies(url)
+})
 
-
-    getFirstMovie();
-    getAllFilms()
-
-    // display movie description
-
-    function getFirstMovie(){
-        fetch(url+'/1')
+//Create fetch function
+function fetchMovies(url){
+    fetch(url)
     .then(response => response.json())
-    .then(film => renderFilm(film));
-    }
-
-    function renderFilm(film){
-        posterDiv.dataset.id = film.id
-        posterDiv.src = film.poster
-        showingDiv.dataset.id = film.id
-        showingDiv.dataset.capacity = film.capacity
-        showingDiv.dataset.tickets_sold = film.tickets_sold
-        showingDiv.querySelector("#title").innerText = film.title
-        showingDiv.querySelector("#film-info").innerText = film.description
-        showingDiv.querySelector("#runtime").innerText = film.runtime +" Minutes"
-        showingDiv.querySelector("#showtime").innerText = film.showtime
-        showingDiv.querySelector("#ticket-num").innerText = parseInt(film.capacity) - parseInt(film.tickets_sold)
-        if (parseInt(film.capacity)-parseInt(film.tickets_sold)<1){
-            tktsBtn.innerText = "Sold Out" 
-            tktsBtn.disabled = true
-            tktsBtn.className = "sold-out"
-        } else {
-            tktsBtn.innerText = "Buy Ticket" 
-            tktsBtn.disabled = false
-            tktsBtn.className = "ui orange button"
-        }
-
-    }
-
-    // one is able to buy a ticket
-
-    tktsBtn.addEventListener('click', function(){
-        let id = showingDiv.dataset.id
-        let capacity = showingDiv.dataset.capacity
-        let tickets_sold = showingDiv.dataset.tickets_sold
-        if (parseInt(capacity)-parseInt(tickets_sold)>0){
-            tickets_sold = parseInt(tickets_sold)+1
-            fetch(url+`/${id}`, {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type':'application/json'
-                },
-                body: JSON.stringify({tickets_sold: tickets_sold})
-            })
-            .then(r => r.json())
-            .then(film => updateTickets(film))
-            function updateTickets(film){
-                showingDiv.dataset.tickets_sold = film.tickets_sold
-                showingDiv.querySelector("#ticket-num").innerText = parseInt(film.capacity) - parseInt(film.tickets_sold)
-                if (parseInt(film.capacity)-parseInt(film.tickets_sold)<1){
-                    tktsBtn.innerText = "Sold Out" 
-                    tktsBtn.disabled = true
-                    tktsBtn.className = "sold-out"
-                } 
-            }
-        }
+    .then(movies => {
+        movies.forEach(movie => {
+            displayMovie(movie)
+        });
     })
+}
 
+function displayMovie(movie){
+   
+    const li = document.createElement('li')
+    li.style.cursor="pointer"
+    li.textContent= (movie.title).toUpperCase()
+    listHolder.appendChild(li)
+    addClickEvent()
+}
+function addClickEvent(){
+    let children=listHolder.children
+    // console.log(children)
 
+    for(let i=0; i<children.length; i++){
+        let child=children[i]
+        // console.log(child)
 
-        // Advanced Deliverable
+        child.addEventListener('click',() => {
+            fetch(`${url}/${i+1}`)
 
-    function getAllFilms(){
-        fetch(url)
-    .then(response => response.json())
-    .then(films => renderAllFilms(films));
-    }
+            .then(res => res.json())
+            .then(movie => {
+                document.getElementById('buy-ticket').textContent = 'Buy Ticket'
+                setUpMovieDetails(movie)
+            })
 
-
-    function renderAllFilms(films){
-        const filmList = filmsDiv.children[0]
-        filmList.innerText = ""
-        const filmListSold = filmsDiv.children[1]
-        filmListSold.innerText = "~Sold Out~"
-
-        films.forEach(film =>{
-            let filmDiv = document.createElement('div')
-            if(parseInt(film.capacity)-parseInt(film.tickets_sold)>1){
-            filmDiv.className = "film" 
-            filmDiv.id = film.id 
-            filmDiv.innerText = `${film.title}`
-            filmList.append(filmDiv)
-            } else {
-            filmDiv.id = film.id
-            filmDiv.className = "sold-out"
-            filmDiv.innerText = `${film.title}`
-            filmListSold.append(filmDiv)
-            }
-            
         })
     }
+}
+function setUpMovieDetails(childMovie){
+    const preview = document.getElementById('poster')
+    preview.src = childMovie.poster;
 
-        //  Click on a movie in the menu to replace the currently displayed movie's details with the new movie's details.
+    const movieTitle = document.querySelector('#title');
+    movieTitle.textContent = childMovie.title;
+    const movieTime = document.querySelector('#runtime');
+    movieTime.textContent = `${childMovie.runtime} minutes`;
+    const movieDescription = document.querySelector('#film-info');
+    movieDescription.textContent = childMovie.description;
+    const showTime = document.querySelector('#showtime')
+    showTime.textContent = childMovie.showtime;
+    const tickets  = document.querySelector('#ticket-num')
+    tickets.textContent = childMovie.capacity -childMovie.tickets_sold;
+}
+const btn = document.getElementById('buy-ticket')
 
-    filmsDiv.addEventListener('click', function(e){
-
-        let id = e.target.id
-        fetch(url+`/${id}`)
-        .then(response => response.json())
-        .then(film => renderFilm(film));
+        btn.addEventListener('click', function(e){
+            let remTickets = document.querySelector('#ticket-num').textContent
+            e.preventDefault()
+            if(remTickets > 0){
+                document.querySelector('#ticket-num').textContent  = remTickets-1
+                
+            }
+            else if(parseInt(remTickets, 10)===0){
+                btn.textContent = 'Sold Out'
+            }
     })
-
-    
-
-
-});
